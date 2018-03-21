2178cb20bece5ac8f8d0ab9b30c70bb9442538b1
Pierluigi Cifani <pcifani@blurredsoftware.com>
2018-03-21 13:30:19 +0100

    Refactor HTTPBin a bit

--- GifWalletKitTests/APIClientTests.swift
+++ GifWalletKitTests/APIClientTests.swift
@@ -8,46 +8,48 @@ import XCTest
 
 /// Full-suite tests are courtesy of our good friends of HTTPBin
 
-private enum HTTPBINHosts: Environment {
-    case production
-    case development
+fileprivate enum HTTPBin {
+    enum Hosts: Environment {
+        case production
+        case development
 
-    fileprivate var baseURL: URL {
-        switch self {
-        case .production:
-            return URL(string: "https://httpbin.org")!
-        case .development:
-            return URL(string: "https://dev.httpbin.org")!
-        }
-    }
-}
-
-private enum HTTPBINAPI: Endpoint {
-    case ip
-    case orderPizza
-
-    var path: String {
-        switch self {
-        case .orderPizza:
-            return "/forms/post"
-        case .ip:
-            return "/ip"
+        fileprivate var baseURL: URL {
+            switch self {
+            case .production:
+                return URL(string: "https://httpbin.org")!
+            case .development:
+                return URL(string: "https://dev.httpbin.org")!
+            }
         }
     }
 
-    var method: HTTPMethod {
-        switch self {
-        case .orderPizza:
-            return .POST
-        default:
-            return .GET
+    enum API: Endpoint {
+        case ip
+        case orderPizza
+
+        var path: String {
+            switch self {
+            case .orderPizza:
+                return "/forms/post"
+            case .ip:
+                return "/ip"
+            }
+        }
+
+        var method: HTTPMethod {
+            switch self {
+            case .orderPizza:
+                return .POST
+            default:
+                return .GET
+            }
         }
     }
 }
 
 class HTTPBINAPITests: XCTestCase {
     func testIPEndpoint() {
-        let getIP = HTTPBINAPI.ip
+        let getIP = HTTPBin.API.ip
         XCTAssert(getIP.path == "/ip")
         XCTAssert(getIP.method == .GET)
     }
@@ -59,14 +61,14 @@ class APIClientTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        apiClient = APIClient(environment: HTTPBINHosts.production)
+        apiClient = APIClient(environment: HTTPBin.Hosts.production)
     }
 
     func testGET() {
 
         let exp = expectation(description: "Fetch Completes")
 
-        apiClient.performRequest(forEndpoint: HTTPBINAPI.ip) { (data, error) in
+        apiClient.performRequest(forEndpoint: HTTPBin.API.ip) { (data, error) in
             XCTAssert(data != nil)
             exp.fulfill()
         }
